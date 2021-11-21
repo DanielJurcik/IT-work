91c936ba357d8ef8473db79dd6a3663ec19c6fea
Daniel <dano.jurcik@gmail.com>
2021-11-21 10:01:37 +0100

    Added route for showing all companies & job offers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,13 @@ Route::get('/', function () {
 
 Route::get('/test', [HomeController::class, 'test']);
 
-Route::get('/insert-task-form', [HomeController::class, 'getInsertForm'])->name('insert-form');
+Route::get('/add-company', [HomeController::class, 'getInsertForm'])->name('insert-form');
 Route::post('/insert-task', [HomeController::class, 'insertTask'])->name('insert');
+Route::post('/insert-company', [HomeController::class, 'insertCompany'])->name('insert-company');
 Route::get('/select-all-tasks', [HomeController::class, 'selectAllTasks'])->name('select-all-tasks');
 Route::get('/select-task/{id}', [HomeController::class, 'selectTask'])->name('select');
 Route::get('/delete-task/{id}', [HomeController::class, 'deleteTask'])->name('delete');
 Route::get('/update-task-form/{id}', [HomeController::class, 'getUpdateForm'])->name('update-form');
 Route::post('/update-task', [HomeController::class, 'updateTask'])->name('update');
+Route::get('/select-all-offers', [HomeController::class, 'selectAllOffers'])->name('select-all-offers');
+Route::get('/companies', [CompanyController::class, 'selectAllCompanies'])->name('select-all-companies');
