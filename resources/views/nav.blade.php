042ae7fe39c38b1d406b964db303079589015c56
Daniel <dano.jurcik@gmail.com>
2021-12-07 21:27:12 +0100

    Deleted unused files

--- resources/views/nav.blade.php
+++
@@ -1,1 +0,0 @@
-<a href="{{ route('insert-form') }}"> Vlozit ulohu</a> | <a href="{{ route('select-all-tasks') }}"> Zoznam vsetkych uloh</a> <br><br>
--- resources/views/select.blade.php
+++
@@ -1,9 +0,0 @@
-@include('nav')
-
-@if($task)
-    ID: {{ $task->id }} <br>
-    Uloha: {{ $task->name }}<br>
-    Pridelena: {{ $task->owner->owner_name }}<br>
-@else
-    Uloha neexistuje
-@endif
